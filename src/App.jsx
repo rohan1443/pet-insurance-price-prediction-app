f34f635d28bd7622a48b7c9538c19621c6dc67b3
Rohan Mazumdar <rohanm@Rohans-MacBook-Pro.local>
2025-05-03 00:39:48 +0800

    updated backend api

--- src/App.jsx
+++ src/App.jsx
@@ -71,13 +71,20 @@ function App() {
     setPredictLoading(true);
     setTimeout(async () => {
       try {
-        const response = await fetch("http://127.0.0.1:8000/predict ", {
+        const response = await fetch("https://pet-insurance-price-prediction-api-production.up.railway.app/predict", {
           method: "POST",
           headers: {
             "Content-Type": "application/json"
           },
           body: JSON.stringify(form)
         });
+        // const response = await fetch("http://127.0.0.1:8000/predict ", {
+        //   method: "POST",
+        //   headers: {
+        //     "Content-Type": "application/json"
+        //   },
+        //   body: JSON.stringify(form)
+        // });
 
         if (!response.ok) {
           throw new Error("Failed to fetch prediction");
